37d6310bafea4296828ec2aa42df3d487a30a1d4
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2021-06-13 13:47:13 +0200

    feat: Added README.md and improved documentation

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Coordinator
  - Gives out client id's to database_wrappers
 ## Usage
-```cargo run --release```
+```shell
+export COORDINATOR.ADDR=0.0.0.0
+export COORDINATOR.PORT=5343
+cargo run --release
+```
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,52 @@
+#![forbid(unsafe_code)]
+#![deny(
+    clippy::needless_borrow,
+    clippy::panic,
+    clippy::unwrap_used,
+    clippy::panic
+)]
+#![deny(unused_imports)]
+#![forbid(missing_docs)]
+//! This crate provides an coordinator, who assigns ID's to requesting services
+//! and requires them to re-new them every 600 seconds
+
 use actix_web::web::Json;
 use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
 use once_cell::sync::Lazy;
 use serde::Serialize;
+use std::env;
 use std::sync::Mutex;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 type Worker = u16;
 type Timestamp = u64;
-const REREG_TIME: u64 = 10;
+const REREG_TIME: u64 = 600;
 
 #[derive(PartialEq, Debug, Copy, Clone)]
 enum STATUS {
     Unused,
     Inuse,
 }
-
+/// Holds response for / request
 #[derive(Serialize)]
-struct Response {
-    id: Worker,
-    ts: Timestamp,
-    re_ts: Timestamp,
+pub struct Response {
+    /// Worker id of requester
+    pub id: Worker,
+    /// Request timestamp
+    pub ts: Timestamp,
+    /// Last accepted timestamp, before id is given out again
+    pub re_ts: Timestamp,
 }
 
 static WORKER_STATUS: Lazy<Mutex<Vec<(Worker, STATUS, Timestamp)>>> =
     Lazy::new(|| Mutex::new(Vec::with_capacity(u16::MAX as usize)));
 
 async fn get_id() -> Result<Json<Response>, ()> {
-    let mut worker_status = WORKER_STATUS.lock().unwrap();
+    let mut worker_status = WORKER_STATUS.lock().expect("Failed to lock WORKER_STATUS mutex");
     let time = SystemTime::now()
         .duration_since(UNIX_EPOCH)
-        .unwrap()
+        .expect("Time went backwards")
         .as_secs();
 
     //Get outdated
@@ -42,7 +59,7 @@ async fn get_id() -> Result<Json<Response>, ()> {
     };
 
     for od in outdated {
-        let idx = { worker_status.iter().position(|f| f == &od).unwrap() };
+        let idx = { worker_status.iter().position(|f| f == &od).expect("Existing item does not exist") };
         worker_status[idx] = (od.0, STATUS::Unused, 0);
     }
 
@@ -57,7 +74,7 @@ async fn get_id() -> Result<Json<Response>, ()> {
     match id {
         None => Err(()),
         Some(idv) => {
-            let idx = worker_status.iter().position(|f| f == &idv).unwrap();
+            let idx = worker_status.iter().position(|f| f == &idv).expect("Existing item does not exist");
 
             worker_status[idx] = (idv.0, STATUS::Inuse, time);
             Ok(web::Json(Response {
@@ -71,8 +88,11 @@ async fn get_id() -> Result<Json<Response>, ()> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().expect("Couldn't load env variables !");
     {
-        let mut worker_status = WORKER_STATUS.lock().unwrap();
+        let mut worker_status = WORKER_STATUS
+            .lock()
+            .expect("Couldn't lock WORKER_STATUS mutex");
         for worker_id in u16::MIN..u16::MAX {
             (*worker_status).push((worker_id, STATUS::Unused, 0));
         }
@@ -82,7 +102,11 @@ async fn main() -> std::io::Result<()> {
         let app = App::new();
         app.service(web::resource("/").route(web::get().to(get_id)))
     })
-    .bind("0.0.0.0:8541")?
+    .bind(format!(
+        "{}:{}",
+        env::var("COORDINATOR.ADDR").expect("Env key COORDINATOR.ADDR not set"),
+        env::var("COORDINATOR.PORT").expect("Env key COORDINATOR.PORT not set")
+    ))?
     .run();
     server.await
 }
